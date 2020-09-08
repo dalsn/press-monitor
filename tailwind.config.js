119291e1b18ba6b94ebaa3b4b5a36244218556b1
geekmaros <razicruz@gmail.com>
2020-09-09 00:02:59 +0100

    initial scaffolded files

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["Inter", "sans-serif"],
+        sans: ["Roboto", "sans"]
+      },
+      fontSize: {
+        9: "0.5625rem",
+        12: "1.18rem",
+        15: "0.9375rem",
+        17: "1.0625rem",
+        51: "3.20571875rem"
+      },
+      colors: {
+        eatizgreen: "#005450",
+        eatizlightgrey: "#999999"
+      },
+      letterSpacing: {
+        23: "0.014375rem"
+      },
+      lineHeight: {
+        19: "19px",
+        24: "24px",
+        25: "25px",
+        26: "26px",
+        28: "28px",
+        88: "88px"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
