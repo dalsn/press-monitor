05b0426d5d8de03e1fb0ea5853a80694117d39be
geekmaros <razicruz@gmail.com>
2020-09-17 08:49:42 +0100

    modified sort button

--- src/views/ResultPageByStateSearch.vue
+++ src/views/ResultPageByStateSearch.vue
@@ -399,7 +399,28 @@
       </div>
       <div class="sort-button w-10/12 flexs grid grid-cols-4 gap-5">
         <SortButton v-for="keys in sortKeys" :key="keys.id" class="w-auto">
-          <span @click="filterQuery = keys.id">{{ keys.name }}</span>
+          <div
+            @click="filterQuery = keys.id"
+            class="flex justify-center items-center"
+          >
+            <span>{{ keys.name }}</span>
+            <svg
+              width="38"
+              height="37"
+              viewBox="0 0 38 37"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g opacity="0.9">
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M10.2263 14.5964C9.68124 15.1415 9.63931 15.9992 10.1005 16.5924L10.2263 16.7348L17.7868 24.2953C18.3319 24.8404 19.1896 24.8824 19.7828 24.4211L19.9253 24.2953L27.4858 16.7348C28.0763 16.1443 28.0763 15.1869 27.4858 14.5964C26.9407 14.0513 26.083 14.0094 25.4898 14.4706L25.3473 14.5964L18.8561 21.0865L12.3648 14.5964C11.8197 14.0513 10.962 14.0094 10.3688 14.4706L10.2263 14.5964Z"
+                  fill="#7E84A3"
+                />
+              </g>
+            </svg>
+          </div>
         </SortButton>
         <!--        <SortButton @click="filterQuery = 'judge'" class="w-auto"-->
         <!--          >Judge</SortButton-->
