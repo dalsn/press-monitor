2b743424170dc5a77fc2e281309d7d185915e626
geekmaros <razicruz@gmail.com>
2020-10-04 21:04:18 +0100

    added recorded

---
+++ src/components/UIElements/BlackDotSvg.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "BlackDotSvg"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/UIElements/LandingStatCard.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "LandingStatCard"
+}
+</script>
+
+<style scoped>
+
+</style>
