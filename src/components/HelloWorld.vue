910d6e00b2f8e464e8f661b616d677492708619d
geekmaros <razicruz@gmail.com>
2020-10-13 10:58:10 +0100

    started mobile responisveness and featured stat

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello font-serif">
     <h1 class="text-3xl">{{ msg }}</h1>
     <p class="text-red-700">
-      For a guide and recipes on how to configure / customize this project,<br />
+      For a guide and recipes on how to configures / customize this project,<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
         >vue-cli documentation</a
