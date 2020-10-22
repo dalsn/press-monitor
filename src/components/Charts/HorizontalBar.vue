ec46090bd6640eeefc9c77d6fc995127cd40f7d0
geekmaros <razicruz@gmail.com>
2020-10-22 18:12:37 +0100

    added public chart

---
+++ src/components/Charts/HorizontalBar.vue
@@ -0,0 +1,22 @@
+<script>
+import { HorizontalBar } from "vue-chartjs";
+export default {
+  name: "HorizontalBar.vue",
+  extends: HorizontalBar,
+  props: {
+    chartdata: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
+  }
+};
+</script>
+
+<style scoped></style>
