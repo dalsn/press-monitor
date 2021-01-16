6a35378c9ae095efa77ab2e0893d3da6edea0973
Dalhatu Njidda <dalsdnjidda@gmail.com>
2021-01-16 17:12:28 +0100

    merge

---
+++ dist/precache-manifest.d350e22930e82cef2b68a64bcae4d34e.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57e35c1c934de3c4537",
+    "url": "/css/app.e0f3980c.css"
+  },
+  {
+    "revision": "bdda274e32a200685196",
+    "url": "/css/explore.ffac4955.css"
+  },
+  {
+    "revision": "892775d546b6a8d34203",
+    "url": "/css/index.1ef3d7dc.css"
+  },
+  {
+    "revision": "80b75aef90ebf42fe157",
+    "url": "/css/state.5f92d0fa.css"
+  },
+  {
+    "revision": "7919768a1ecd2478c31bf3c33d4d519f",
+    "url": "/img/blackdot.7919768a.png"
+  },
+  {
+    "revision": "461c1ff5ab84ce519ead7a9ae717e652",
+    "url": "/img/courtcases.461c1ff5.svg"
+  },
+  {
+    "revision": "ad79dbe18e22673242d514c545e12485",
+    "url": "/img/dfid.ad79dbe1.png"
+  },
+  {
+    "revision": "3cbcee3a02410b40bc699292539f57f2",
+    "url": "/img/heroill.3cbcee3a.svg"
+  },
+  {
+    "revision": "ad63be7bcf4b7d2e76b796cd9ffa1643",
+    "url": "/img/justice.ad63be7b.svg"
+  },
+  {
+    "revision": "d0e7be1353ae8a7a56ab7062f9aae9b3",
+    "url": "/img/scale.d0e7be13.svg"
+  },
+  {
+    "revision": "a04d174fa76cd012b9fb97387ff4eb9b",
+    "url": "/img/search-by-algolia.a04d174f.svg"
+  },
+  {
+    "revision": "4389129ca5fbf76f20bdcfc8a4790a7d",
+    "url": "/img/staticon.4389129c.svg"
+  },
+  {
+    "revision": "4dd263c6c61d0986d56e52cabc335a7e",
+    "url": "/img/tcclogo_white.4dd263c6.svg"
+  },
+  {
+    "revision": "47ad2cecbb9732589f641ea23bd88797",
+    "url": "/img/topic.47ad2cec.svg"
+  },
+  {
+    "revision": "c675aad1c739c1c52e3e91608a3b1461",
+    "url": "/img/transparencit-cropped.c675aad1.png"
+  },
+  {
+    "revision": "de2e031ccff170e4fe84b764651dd187",
+    "url": "/img/trustafrica.de2e031c.png"
+  },
+  {
+    "revision": "69da24762c8f549d2a9b96e1fdc8cb14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abc2d78ea54693fcfdc9",
+    "url": "/js/about.4c7f4257.js"
+  },
+  {
+    "revision": "d57e35c1c934de3c4537",
+    "url": "/js/app.fc05668f.js"
+  },
+  {
+    "revision": "2a608b395adcbcd47d9a",
+    "url": "/js/chunk-vendors.e4a1747f.js"
+  },
+  {
+    "revision": "e7a2c5949c091cccddef",
+    "url": "/js/datapoint.b262f612.js"
+  },
+  {
+    "revision": "1241b08a31183640392d",
+    "url": "/js/datapoint~explore~index.7c0516c5.js"
+  },
+  {
+    "revision": "bdda274e32a200685196",
+    "url": "/js/explore.4541785b.js"
+  },
+  {
+    "revision": "6c420c4b194cedaffeb4",
+    "url": "/js/explore~index.bffb20d9.js"
+  },
+  {
+    "revision": "892775d546b6a8d34203",
+    "url": "/js/index.4ebef43c.js"
+  },
+  {
+    "revision": "80b75aef90ebf42fe157",
+    "url": "/js/state.d1ad3bb2.js"
+  },
+  {
+    "revision": "f8dcc357eea7a3da26eb7f4bb3fdf969",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
