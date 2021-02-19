3f1737a9762c2567dce0cd9414e1ffa899ba5795
Dalhatu Njidda <dalsdnjidda@gmail.com>
2021-02-19 18:36:06 +0100

    fix

--- src/components/UIElements/CaseCard.vue
+++ src/components/UIElements/CaseCard.vue
@@ -1,41 +1,44 @@
 <template>
   <div
-    class="flex shadow-lg rounded-lg px-5 py-4 md:pt-8 md:pb-4 mb-8 border case-card"
+    class="flex flex-col shadow-lg rounded-lg px-5 py-4 md:pt-8 md:pb-4 mb-8 border case-card justify-between"
     :class="className"
   >
-    <div>
-      <div class="flex items-center">
-        <hr class="border-2 w-1/6" />
-        <p class="text-lg md:text-2xl px-3 leading-tight">
-          <!-- <router-link :to="offenceUrl(caseFile.type_id)">{{
-            caseFile.type
-          }}</router-link> -->
-          {{ caseFile.type }}
-        </p>
-      </div>
-      <p class="text-lg md:text-xl lg:text-2xl my-4">
-        <router-link :to="'cases/' + caseFile.slug">{{
-          caseFile.title
-        }}</router-link>
+    <!-- <div> -->
+    <div class="flex items-center">
+      <hr class="border-2 w-1/6" />
+      <p class="text-lg md:text-2xl px-3 leading-tight">
+        <!-- <router-link :to="offenceUrl(caseFile.type_id)">{{
+          caseFile.type
+        }}</router-link> -->
+        {{ caseFile.type }}
       </p>
-      <p class="text-sm md:text-lg mb-1">
-        <strong>Prosecuting Agency:</strong> {{ caseFile.agency.name }}
-      </p>
-      <p class="text-sm md:text-lg mb-4">
-        <strong>Presiding Judge:</strong> {{ caseFile.judge.name }}
-      </p>
-      <div class="flex items-center justify-between">
-        <button
-          class="text-sm md:text-md btn w-2/5"
-          @click="gotoCase(caseFile.slug)"
-        >
-          Read More
-        </button>
-        <p class="text-right text-sm font-semibold">
-          Status: {{ caseFile.status }}
-        </p>
-      </div>
     </div>
+
+    <!-- <div> -->
+    <p class="text-lg md:text-xl lg:text-2xl my-4">
+      <router-link :to="'cases/' + caseFile.slug">{{
+        caseFile.title
+      }}</router-link>
+    </p>
+    <p class="text-sm md:text-lg mb-1">
+      <strong>Prosecuting Agency:</strong> {{ caseFile.agency.name }}
+    </p>
+    <p class="text-sm md:text-lg mb-4">
+      <strong>Presiding Judge:</strong> {{ caseFile.judge.name }}
+    </p>
+    <!-- </div> -->
+    <div class="flex items-center justify-between">
+      <button
+        class="text-sm md:text-md btn w-2/5"
+        @click="gotoCase(caseFile.slug)"
+      >
+        Read More
+      </button>
+      <p class="text-right text-sm font-semibold">
+        Status: {{ caseFile.status }}
+      </p>
+    </div>
+    <!-- </div> -->
   </div>
 </template>
 
